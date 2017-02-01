b4ee39dcc6b86b338cb8bac070c612161932b8d0
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-01-31 19:20:47 -0500

    Response

--- src/index.js
+++ src/index.js
@@ -7,3 +7,40 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+var Stopwatch = React.createClass({
+  getInitialState: function(){
+    return { secondsElapsed: 0 };
+  },
+  getSeconds: function () {
+    return ('0' + this.state.secondsElapsed % 60).slice(-2);
+  },
+  getMinutes: function () {
+    return Math.floor(this.state.secondsElapsed / 60);
+  },
+handleStartClick: function () {
+  var _this = this;
+
+  this.incrementer = setInterval(function (){
+    _this.setState({
+      secondsElapsed:
+    (_this.state.secondsElapsed + 1)
+    });
+  }, 1000)
+},
+handleStopClick: function () {
+  clearInterval(this.incrementer);
+},
+  render: function (){
+    return <div>
+    <h1>{this.getMinutes()}:{this.getSeconds()}</h1>
+      <button type="button" onClick={this.handleStartClick}>start</button>
+      <button type="button" onClick={this.handleStopClick}>stop</button>
+  </div>
+}
+});
+
+ReactDOM.render(
+  <Stopwatch />,
+  document.getElementById('root')
+);
